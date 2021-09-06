d9efe98fd69d53a48b65a47bcfcd03063fed2b2e
N-Sridhar <nsridharbtech@gmail.com>
2021-09-06 17:50:03 +0530

    Proxy Pattern

---
+++ src/com/sri/patterns/proxy/Customer.java
@@ -0,0 +1,10 @@
+package com.sri.patterns.proxy;
+
+import java.util.List;
+
+public interface Customer {
+
+	int getId();
+
+	List<Order> getOrders();
+}
---
+++ src/com/sri/patterns/proxy/CustomerProxyImpl.java
@@ -0,0 +1,34 @@
+package com.sri.patterns.proxy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerProxyImpl implements Customer {
+
+	Customer customer = new CustomerImpl();
+
+	@Override
+	public int getId() {
+		return customer.getId();
+	}
+
+	@Override
+	public List<Order> getOrders() {
+		List<Order> orders = new ArrayList<>();
+
+		Order order1 = new Order();
+		order1.setId(1);
+		order1.setProductName("iPhone");
+		order1.setQuantity(2);
+		orders.add(order1);
+
+		Order order2 = new Order();
+		order2.setId(2);
+		order2.setProductName("Mac");
+		order2.setQuantity(1);
+		orders.add(order2);
+
+		return orders;
+	}
+
+}
